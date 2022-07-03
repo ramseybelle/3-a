814312861e965a52a19a9c0f16de4eb68d296779
ramseybelle <108037203+ramseybelle@users.noreply.github.com>
2022-07-02 19:45:30 -0700

    eh

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ small: int = 999999999999999  # initialize small with some large number.
 large: int = -99999999999999  # initialize large with some lease number.
 for i in range(0, n):  # loop will run up to a number of integers.
     k: int = int(input())  # read the integer one by one.
-if 'k' < small:  # if the integer is less than the small.
-    small = 'k'  # stores the integer in small
-if 'k' > large:  # if the integer is more than the large.
-    large = 'k'  # stores the integer in large
+if k < small:  # if the integer is less than the small.
+    small = k  # stores the integer in small
+if k > large:  # if the integer is more than the large.
+    large = k  # stores the integer in large
 print("min:", small)  # printing the small value
 print("max:", large)  # print the large value.
